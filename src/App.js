e919c6ddc5527b8e7e6b12175fd6ac9fbe56f9c1
rajaneehs <rrajaneesh5@gmail.com>
2020-02-19 08:34:23 +0530

    other changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+// react-icons
+// import IconsComponent from "./icons/IconsComponent";
+
+// toast notifications
+// import NotificationsComponent from './notifications/NotificationsComponent'
+
+// modal
+import ModalComponent from './modal/ModalComponent'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <IconsComponent />
+      <NotificationsComponent /> */}
+      <ModalComponent />
     </div>
   );
 }
